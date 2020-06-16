aa5849e30f4fddaf20c100d9ed0a6ef00da741da
Jayaram Subramanian <js0003@mix.wvu.edu>
2020-06-16 16:43:57 -0400

    Update server.py
    Back to original

--- app/server.py
+++ app/server.py
@@ -9,11 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1BpLsy4Us3YTdlv3e1qFX0vwoGzRWiWsn/view?usp=sharing'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classTamil = list(range(0,156))
-classes = classTamil
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
